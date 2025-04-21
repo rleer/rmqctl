957e4f7c770ba26a4eb4880f68d8a5c34f853e8d
Richard Leer <richard.leer@parshipgroup.com>
2025-04-21 16:09:48 +0200

    Formatting

--- src/rmqctl/Commands/PublishCommandHandler.cs
+++ src/rmqctl/Commands/PublishCommandHandler.cs
@@ -19,30 +19,30 @@ public class PublishCommandHandler : ICommandHandler
     public void Configure(RootCommand rootCommand)
     {
         _logger.LogDebug("Configuring publish command...");
-        
+
         var publishCommand = new Command("publish", "Publish message to a queue");
-        
+
         var queueOption = new Option<string>("--queue", "Queue name to send message to");
         queueOption.AddAlias("-q");
-        
+
         var exchangeOption = new Option<string>("--exchange", "Exchange name to send message to");
         exchangeOption.AddAlias("-e");
         exchangeOption.SetDefaultValue("amq.direct");
-        
+
         var routingKeyOption = new Option<string>("--routing-key", "Routing key to send message to");
         routingKeyOption.AddAlias("-r");
 
         var messageOption = new Option<string>("--message", "Message to send");
         messageOption.AddAlias("-m");
-            
+
         var fromFileOption = new Option<string>("--from-file", "Path to a file that contains the message body to send");
-        
+
         publishCommand.AddOption(queueOption);
         publishCommand.AddOption(exchangeOption);
         publishCommand.AddOption(routingKeyOption);
         publishCommand.AddOption(messageOption);
         publishCommand.AddOption(fromFileOption);
-        
+
         publishCommand.AddValidator(result =>
         {
             if (result.GetValueForOption(queueOption) is null && result.GetValueForOption(routingKeyOption) is null)
@@ -54,13 +54,19 @@ public class PublishCommandHandler : ICommandHandler
             {
                 result.ErrorMessage = "You must specify a message to send or a file that contains the message body.";
             }
+
             if (result.GetValueForOption(fromFileOption) is not null && result.GetValueForOption(messageOption) is not null)
             {
                 result.ErrorMessage = "You cannot specify both a message and a file that contains the message body.";
             }
         });
-        publishCommand.SetHandler(Handle, new DestinationBinder(queueOption, exchangeOption, routingKeyOption), messageOption, fromFileOption);
-        
+        publishCommand.SetHandler(
+            Handle,
+            new DestinationBinder(queueOption, exchangeOption, routingKeyOption),
+            messageOption,
+            fromFileOption
+        );
+
         rootCommand.AddCommand(publishCommand);
     }
 
