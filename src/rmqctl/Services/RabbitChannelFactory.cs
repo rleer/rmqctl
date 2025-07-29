2257268f29898ac75c04f0b2c590ef5fed3fb0e6
Richard Leer <richard.leer@parshipgroup.com>
2025-07-29 11:42:56 +0200

    Extend channel factory to create channel with publisher confirms
    Allows better error handling in case user tries sending messages to non-existing exchanges/routing-keys/queues

--- src/rmqctl/Services/RabbitChannelFactory.cs
+++ src/rmqctl/Services/RabbitChannelFactory.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.Logging;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using rmqctl.Configuration;
 
 namespace rmqctl.Services;
@@ -7,6 +8,7 @@ namespace rmqctl.Services;
 public interface IRabbitChannelFactory
 {
     Task<IChannel> GetChannelAsync();
+    Task<IChannel> GetChannelWithPublisherConfirmsAsync();
 }
 
 public class RabbitChannelFactory : IRabbitChannelFactory
@@ -37,31 +39,48 @@ public class RabbitChannelFactory : IRabbitChannelFactory
         return await GetChannelAsync(connection);
     }
 
-    // TODO: Pass cancellation token
+    public async Task<IChannel> GetChannelWithPublisherConfirmsAsync()
+    {
+        var connection = await GetConnectionAsync();
+
+        _logger.LogDebug("Acquiring RabbitMQ channel with publisher confirmations enabled");
+        var channelOptions = new CreateChannelOptions(
+            publisherConfirmationsEnabled: true,
+            publisherConfirmationTrackingEnabled: true,
+            outstandingPublisherConfirmationsRateLimiter: new ThrottlingRateLimiter(256)); // TODO: Should be configurable?
+        var channel = await connection.CreateChannelAsync(channelOptions);
+
+        // Register event handlers for channel events
+        channel.BasicReturnAsync += OnChannelOnBasicReturnAsync;
+        channel.ChannelShutdownAsync += OnChannelOnChannelShutdownAsync;
+
+        return channel;
+    }
+
     private async Task<IChannel> GetChannelAsync(IConnection connection)
     {
         _logger.LogDebug("Acquiring RabbitMQ channel");
         var channel = await connection.CreateChannelAsync();
 
-        channel.BasicReturnAsync += (_, @event) =>
-        {
-            _logger.LogWarning("Message returned: {ReplyCode} - {ReplyText}", @event.ReplyCode, @event.ReplyText);
-            return Task.CompletedTask;
-        };
+        // Register event handlers for channel events
+        channel.BasicReturnAsync += OnChannelOnBasicReturnAsync;
+        channel.ChannelShutdownAsync += OnChannelOnChannelShutdownAsync;
 
         return channel;
     }
 
     private async Task<IConnection> GetConnectionAsync()
     {
-        if (_connection != null && _connection.IsOpen)
+        if (_connection is { IsOpen: true })
+        {
+            _logger.LogDebug("Reusing existing RabbitMQ connection");
             return _connection;
+        }
 
-        _logger.LogDebug("Connecting to RabbitMQ, host={Host}, port={Port}, virtual host={VirtualHost}, client name={ClientName}", 
+        _logger.LogDebug("Connecting to RabbitMQ, host={Host}, port={Port}, vhost={VirtualHost}, client={ClientName}",
             _config.Host, _config.Port, _config.VirtualHost, _config.ClientName);
         _connection = await _connectionFactory.CreateConnectionAsync();
 
-        // Set up connection shutdown event handler
         _connection.ConnectionShutdownAsync += (_, args) =>
         {
             _logger.LogWarning("RabbitMQ connection shut down: {Reason}", args.ReplyText);
@@ -69,5 +88,17 @@ public class RabbitChannelFactory : IRabbitChannelFactory
         };
 
         return _connection;
+    }
+
+    private Task OnChannelOnChannelShutdownAsync(object _, ShutdownEventArgs @event)
+    {
+        _logger.LogInformation("RabbitMQ channel shut down: {ReplyText}", @event.ReplyText);
+        return Task.CompletedTask;
+    }
+
+    private Task OnChannelOnBasicReturnAsync(object _, BasicReturnEventArgs @event)
+    {
+        _logger.LogWarning("Message returned: {ReplyCode} - {ReplyText}", @event.ReplyCode, @event.ReplyText);
+        return Task.CompletedTask;
     }
 }
